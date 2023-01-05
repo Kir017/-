e4cbfef25aa289913ffd7ef938b29b02542fbb4d
Kirill Kovalev <kir017@mail.ru>
2023-01-05 23:03:05 +0300

    Написал код для второй задачи в task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int[] array = {20, 40, 21};
+int index = 0;
+int max = 0;
+while (index < array.Length -1)
+{
+  if (array[index] > max)
+  {
+    max = array[index]; 
+  }
+index = index + 1;
+}
+
+Console.WriteLine("максимум " + max);
