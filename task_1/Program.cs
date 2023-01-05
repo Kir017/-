55a2b31c0f2e9371f6a3198d5e250049bd8a5aee
Kirill Kovalev <kir017@mail.ru>
2023-01-05 21:58:53 +0300

    Написан код

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = -1;
+int b = 1;
+int max = a;
+int min = b;
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+  max = b;
+  min = a;
+}
+
+Console.WriteLine("наименьшее " + min);
+Console.WriteLine("наибольшее " + max);
